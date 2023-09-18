a836adf82f50c25f23a3a8fd816963156f588161
hsnadeeb <hsnadeeb007@gmail.com>
2023-09-18 19:29:50 +0530

    made it a little scalable

--- src/App.js
+++ src/App.js
@@ -35,30 +35,15 @@ function App() {
   return (
     <div>
       <h2>Expense Tracker</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-        location={expenses[0].location}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        location={expenses[1].location}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        location={expenses[2].location}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        location={expenses[3].location}
-      ></ExpenseItem>
+      {expenses.map ((expense)=>(
+        <ExpenseItem
+        id={expense.id}
+        title= {expense.title}
+        amount= {expense.amount}
+        date= {expense.date}
+        location= {expense.location}
+        />
+      ))}
     </div>
   );
 }
