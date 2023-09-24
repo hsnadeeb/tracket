3173409f4c54883d0a0707984b68f139db8c3f13
hsnadeeb <hsnadeeb007@gmail.com>
2023-09-24 15:22:22 +0530

    made the submit button functional

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,44 +1,56 @@
-// import react from 'react';
-const ExpenseForm = (props)=>{
+import React, { useState } from 'react';
 
-    const handleTitleChange = (event) => {
-       
-        console.log(event.target.value);
-      };
-    
-      const handleAmountChange = (event) => {
-    
-        console.log(event.target.value);
-      };
-    
-      const handleDateChange = (event) => {
-    
-        console.log(event.target.value);
-      };
+const ExpenseForm = (props) => {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
 
+  const handleTitleChange = (event) => {
+    const newTitle = event.target.value;
+    setTitle(newTitle);
+  };
 
-    return(
-       <div>
-        <form>
-            <div>
-                <label>Expense Title</label>
-                <input type='text' onChange={handleTitleChange}></input>
-            </div>
-            <div>
-                <label>Expense Amount</label>
-                <input type='number' onChange={handleAmountChange}></input>
-            </div>
-            <div>
-                <label>Date</label>
-                <input type='date' onChange={handleDateChange}></input>
-            </div>
-            <div>
-                <button type='submit'>Submit</button>
-            </div>
-        </form>
-       </div>
+  const handleAmountChange = (event) => {
+    const newAmount = event.target.value;
+    setAmount(newAmount);
+  };
 
-    )
-}
+  const handleDateChange = (event) => {
+    const newDate = event.target.value;
+    setDate(newDate);
+  };
+
+  const handleSubmitChange = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: title,
+      amount: amount,
+      date: new Date(date),
+    };
+    console.log(expenseData);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmitChange}>
+        <div>
+          <label>Expense Title</label>
+          <input type="text" value={title} onChange={handleTitleChange} />
+        </div>
+        <div>
+          <label>Expense Amount</label>
+          <input type="number" value={amount} onChange={handleAmountChange} />
+        </div>
+        <div>
+          <label>Date</label>
+          <input type="date" value={date} onChange={handleDateChange} />
+        </div>
+        <div>
+          <button type="submit">Add Expense</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default ExpenseForm;
