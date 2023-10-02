59af7e4c03cbf07316143debd28fd39984b660b7
hsnadeeb <hsnadeeb007@gmail.com>
2023-10-02 20:02:16 +0530

    made the form visible and not visible using btn

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,6 +5,7 @@ const ExpenseForm = (props) => {
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState('');
   const [date, setDate] = useState('');
+  const [isEditing, setIsEditing] = useState(false); // State to control form visibility
 
   const handleTitleChange = (event) => {
     const newTitle = event.target.value;
@@ -32,29 +33,44 @@ const ExpenseForm = (props) => {
     setAmount('');
     setDate('');
     setTitle('');
+    setIsEditing(false); // Hide the form after submission
   };
+
+  // Toggle form visibility when clicking "Add Expense" button
+  const handleAddExpenseClick = () => {
+    setIsEditing(true);
+  };
+
 
   return (
     <div>
-      <form onSubmit={handleSubmitChange}>
-        <div className='new-expense__controls'>
-        <div className='new-expense__control'>
-          <label>Expense Title</label>
-          <input type="text" value={title} onChange={handleTitleChange} />
-        </div>
-        <div className='new-expense__control'>
-          <label>Expense Amount</label>
-          <input type="number" value={amount} onChange={handleAmountChange} />
-        </div>
-        <div className='new-expense__control'>
-          <label>Date</label>
-          <input type="date" value={date} onChange={handleDateChange} />
-        </div>
-        <div className='new-expense__actions'>
-          <button type="submit">Add Expense</button>
-        </div>
-        </div>
-      </form>
+      {!isEditing && (
+        <button className='new-expense__actions' onClick={handleAddExpenseClick}>
+          Add Expense
+        </button>
+      )}
+      {isEditing && (
+        <form onSubmit={handleSubmitChange}>
+          <div className='new-expense__controls'>
+            <div className='new-expense__control'>
+              <label>Expense Title</label>
+              <input type="text" value={title} onChange={handleTitleChange} />
+            </div>
+            <div className='new-expense__control'>
+              <label>Expense Amount</label>
+              <input type="number" value={amount} onChange={handleAmountChange} />
+            </div>
+            <div className='new-expense__control'>
+              <label>Date</label>
+              <input type="date" value={date} onChange={handleDateChange} />
+            </div>
+            <div className='new-expense__actions'>
+              <button type="submit">Add Expense</button>
+              <button onClick={() => setIsEditing(false)}>Cancel</button>
+            </div>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
